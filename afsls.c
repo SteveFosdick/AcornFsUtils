9893fd6ca5ecd127865964e34655348fc1e9e4e8
Steve Fosdick <steve@fosdick.me.uk>
2019-10-28 00:40:05 +0000

    Only list the names of matching directories.

--- afsls.c
+++ afsls.c
@@ -2,29 +2,11 @@
 #include <string.h>
 #include <locale.h>
 
-static int callback_simple(acorn_fs *fs, acorn_fs_object *obj, void *udata, const char *path)
+static int callback(acorn_fs *fs, acorn_fs_object *obj, void *udata, const char *path)
 {
     acorn_fs_info(obj, stdout);
     printf(" %s\n", path);
     return AFS_OK;
-}
-
-static int callback_print(acorn_fs *fs, acorn_fs_object *obj, void *udata, const char *path)
-{
-    acorn_fs_info(obj, stdout);
-    printf(" %s.%s\n", (char *)udata, path);
-    return AFS_OK;
-}
-
-static int callback_dodir(acorn_fs *fs, acorn_fs_object *obj, void *udata, const char *path)
-{
-    if (obj->is_dir)
-        return fs->glob(fs, obj, "*", callback_print, (void *)path);
-    else {
-        acorn_fs_info(obj, stdout);
-        printf(" %s\n", path);
-        return AFS_OK;
-    }
 }
 
 int main(int argc, char *argv[])
@@ -39,11 +21,7 @@ int main(int argc, char *argv[])
             if (sep)
                 *sep++ = 0;
             if ((fs = acorn_fs_open(fsname, false))) {
-                int astat;
-                if (sep)
-                    astat = fs->glob(fs, NULL, sep, callback_dodir, NULL);
-                else
-                    astat = fs->glob(fs, NULL, "*", callback_simple, NULL);
+                int astat = fs->glob(fs, NULL, sep ? sep : "*", callback, NULL);
                 if (astat != AFS_OK) {
                     fprintf(stderr, "afsls: %s: %s\n", fsname, acorn_fs_strerr(astat));
                     status++;
