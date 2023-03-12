2640fecd61d11d95a61620fae6763c4984e9dc32
Steve Fosdick <steve@fosdick.me.uk>
2023-03-12 01:45:23 +0000

    Fix copying of zero-length files.

--- afscp.c
+++ afscp.c
@@ -170,7 +170,7 @@ static int native_save(acorn_fs_object *obj, const char *filename)
     int status;
     FILE *fp = fopen(filename, "wb");
     if (fp) {
-        if (fwrite(obj->data, obj->length, 1, fp) == 1)
+        if (!obj->length || fwrite(obj->data, obj->length, 1, fp) == 1)
             status = write_inf(obj, filename);
         else {
             status = errno;
